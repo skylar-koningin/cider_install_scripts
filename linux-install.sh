897d084613a19a78d2023389afce7d88178e04c5
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-15 00:22:43 -0400

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+
+export PACMAN="$HOME/cider_install_scripts/Linux/ArchLinux/"
+export APTITUDE="$HOME/cider_install_scripts/Linux/Debian/"
+export DNF="$HOME/cider_install_scripts/Linux/Fedora/"
+export OTHERDISTRO="$HOME/cider_install_scripts/Linux/OtherDistro/"
+
 distro_choose() {
-echo "Choose a distro to install"
-echo "1. Arch Linux"
-echo "2. Debian"
-echo "3. Fedora"
+echo "Choose the package manager your system uses."
+echo "1. Pacman (ArchLinux-based distros)"
+echo "2. Aptitude (Debian/Ubuntu-based distros)"
+echo "3. DNF (Fedora-based distros)"
 echo "4. Anything else"
-read -p "Choose a distro (choose 0 to abort): " num
+read -p "Select the number (choose 0 to abort): " num
 }
 
-archlinux_script() {
+pacman_script() {
 sudo pacman -Syu
-cd $HOME/cider_install_scripts/Linux/ArchLinux/
+cd $PACMAN
 curl -s https://repo.cider.sh/ARCH-GPG-KEY | sudo pacman-key --add -
 sudo pacman-key --lsign-key A0CD6B993438E22634450CDD2A236C3F42A61682
 sudo tee -a /etc/pacman.conf > /dev/null <<EOF
@@ -24,45 +30,45 @@ sudo pacman -Sy
 sudo pacman -U cider-v3.0.0-linux-x64.pkg.tar.zst
 sudo pacman -S cider
 echo "Cider has been installed!"
-sudo rm ./cider-v3.0.0-linux-x64.pkg.tar.zst
+sudo rm $PACMAN/cider-v3.0.0-linux-x64.pkg.tar.zst
 cd $HOME/
 exit 0
 }
 
-debian_script() {
+aptitude_script() {
 sudo apt update && sudo apt upgrade
-cd $HOME/cider_install_scripts/Linux/Debian/
+cd $APTITUDE
 curl -fsSL https://repo.cider.sh/APT-GPG-KEY | sudo gpg --dearmor -o /usr/share/keyrings/cider-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cider-archive-keyring.gpg] https://repo.cider.sh/apt stable main" | sudo tee /etc/apt/sources.list.d/cider.list
 sudo apt update && sudo apt upgrade && sudo dpkg -i cider-v3.0.2-linux-x64.deb
 sudo apt install cider
 echo "Cider has been installed!"
-sudo rm ./cider-v3.0.2-linux-x64.deb
+sudo rm $APTITUDE/cider-v3.0.2-linux-x64.deb
 cd $HOME/
 exit 0
 }
 
-fedora_script() {
+dnf_script() {
 sudo dnf upgrade
-cd $HOME/cider_install_scripts/Linux/Fedora/
+cd $DNF
 sudo rpm --import https://repo.cider.sh/RPM-GPG-KEY
-sudo mv ./cider.repo /etc/yum.repos.d
+sudo mv $DNF/cider.repo /etc/yum.repos.d
 sudo dnf makecache
 sudo rpm -i cider cider-v3.0.2-linux-x64.rpm
 sudo dnf cider
 echo "Cider has been installed!"
-sudo rm ./cider cider-v3.0.2-linux-x64.rpm
+sudo rm $DNF/cider cider-v3.0.2-linux-x64.rpm
 cd $HOME/
 exit 0
 }
 
 otherdistro_script() {
 cd $HOME/cider_install_scripts/Linux/OtherDistro/
-chmod +x ./cider-v3.0.2-linux-x64.AppImage
+chmod +x $OTHERDISTRO/cider-v3.0.2-linux-x64.AppImage
 mkdir $HOME/Cider
-sudo mv ./512.png $HOME/Cider
-sudo mv ./cider-v3.0.2-linux-x64.AppImage $HOME/Cider
-sudo mv ./cider.desktop $HOME/.local/share/applications/
+sudo mv $OTHERDISTRO/512.png $HOME/Cider
+sudo mv $OTHERDISTRO/cider-v3.0.2-linux-x64.AppImage $HOME/Cider
+sudo mv $OTHERDISTRO/cider.desktop $HOME/.local/share/applications/
 chmod +x $HOME/.local/share/applications/cider.desktop
 exit 0
 }
@@ -81,17 +87,17 @@ fi
 
 if [[ "$num" =~ ^[1]+$ ]]; then
 echo "Chose Arch Linux"
-archlinux_script
+pacman_script
 fi
 
 if [[ "$num" =~ ^[2]+$ ]]; then
 echo "Chose Debian"
-debian_script
+aptitude_script
 fi
 
 if [[ "$num" =~ ^[3]+$ ]]; then
 echo "Chose Fedora"
-fedora_script
+dnf_script
 fi
 
 if [[ "$num" =~ ^[4]+$ ]]; then

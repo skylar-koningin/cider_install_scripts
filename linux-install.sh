eee38b54e622f0e9f71bb599549f5aca33fa5a15
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-15 13:57:18 -0400

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export PACMAN="$SUDO_HOME/cider_install_scripts/Linux/Pacman"
-export APTITUDE="$SUDO_HOME/cider_install_scripts/Linux/Aptitude"
-export DNF="$SUDO_HOME/cider_install_scripts/Linux/DNF"
-export ZYPPER="$SUDO_HOME/cider_install_scripts/Linux/Zypper"
-export OTHERDISTRO="$SUDO_HOME/cider_install_scripts/Linux/OtherDistro"
-
 distro_choose() {
 echo "Choose the package manager your system uses."
 echo "1. Pacman (ArchLinux-based distros)"
@@ -17,7 +11,7 @@ read -p "Select the number (choose 0 to abort): " num
 
 pacman_script() {
 sudo pacman -Syu
-cd $PACMAN
+cd ~/cider_install_scripts/Linux/Pacman
 curl -s https://repo.cider.sh/ARCH-GPG-KEY | sudo pacman-key --add -
 sudo pacman-key --lsign-key A0CD6B993438E22634450CDD2A236C3F42A61682
 sudo tee -a /etc/pacman.conf > /dev/null <<EOF
@@ -31,46 +25,46 @@ sudo pacman -Sy
 sudo pacman -U cider-v3.0.0-linux-x64.pkg.tar.zst
 sudo pacman -S cider
 echo "Cider has been installed!"
-sudo rm $PACMAN/cider-v3.0.0-linux-x64.pkg.tar.zst
-cd $SUDO_HOME/
+sudo rm ~/cider_install_scripts/Linux/Pacmancider-v3.0.0-linux-x64.pkg.tar.zst
+cd ~/
 exit 0
 }
 
 aptitude_script() {
 sudo apt update && sudo apt upgrade
-cd $APTITUDE
+cd ~/cider_install_scripts/Linux/Aptitude
 curl -fsSL https://repo.cider.sh/APT-GPG-KEY | sudo gpg --dearmor -o /usr/share/keyrings/cider-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cider-archive-keyring.gpg] https://repo.cider.sh/apt stable main" | sudo tee /etc/apt/sources.list.d/cider.list
 sudo apt update && sudo apt upgrade && sudo dpkg -i cider-v3.0.2-linux-x64.deb
 sudo apt install cider
 echo "Cider has been installed!"
-sudo rm $APTITUDE/cider-v3.0.2-linux-x64.deb
-cd $SUDO_HOME/
+sudo rm ~/cider_install_scripts/Linux/Aptitude/cider-v3.0.2-linux-x64.deb
+cd ~//
 exit 0
 }
 
 dnf_script() {
 sudo dnf upgrade
-cd $DNF
+cd ~/cider_install_scripts/Linux/DNF
 sudo rpm --import https://repo.cider.sh/RPM-GPG-KEY
-sudo mv $DNF/cider.repo /etc/yum.repos.d
+sudo mv ~/cider_install_scripts/Linux/DNF/cider.repo /etc/yum.repos.d
 sudo dnf makecache
 sudo rpm -i cider cider-v3.0.2-linux-x64.rpm
 sudo dnf Cider
 echo "Cider has been installed!"
-sudo rm $DNF/cider-v3.0.2-linux-x64.rpm
-cd $SUDO_HOME/
+sudo rm ~/cider_install_scripts/Linux/DNF/cider-v3.0.2-linux-x64.rpm
+cd ~//
 exit 0
 }
 
 otherdistro_script() {
-cd $SUDO_HOME/cider_install_scripts/Linux/OtherDistro/
-chmod +x $OTHERDISTRO/cider-v3.0.2-linux-x64.AppImage
-mkdir $SUDO_HOME/Cider
-sudo mv $OTHERDISTRO/cider.png $SUDO_HOME/Cider
-sudo mv $OTHERDISTRO/cider-v3.0.2-linux-x64.AppImage $SUDO_HOME/Cider
-sudo mv $OTHERDISTRO/cider.desktop $SUDO_HOME/.local/share/applications/
-chmod +x $SUDO_HOME/.local/share/applications/cider.desktop
+cd ~//cider_install_scripts/Linux/OtherDistro/
+chmod +x ~/cider_install_scripts/Linux/OtherDistro/cider-v3.0.2-linux-x64.AppImage
+mkdir ~//Cider
+sudo mv ~/cider_install_scripts/Linux/OtherDistro/cider.png ~//Cider
+sudo mv ~/cider_install_scripts/Linux/OtherDistro/cider-v3.0.2-linux-x64.AppImage ~//Cider
+sudo mv ~/cider_install_scripts/Linux/OtherDistro/cider.desktop ~//.local/share/applications/
+chmod +x ~//.local/share/applications/cider.desktop
 exit 0
 }
 

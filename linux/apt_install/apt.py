f5d18108154b20416bbeb936be06936fc96ddca8
skylarkoningin <greekie.via.linux@gmail.com>
2025-07-16 15:55:17 -0400

    debugging

--- linux/apt_install/apt.py
+++ linux/apt_install/apt.py
@@ -2,11 +2,11 @@ def apt_install():
     print("Selected apt package manager")
     import subprocess
     try:
-        subprocess.run("sudo apt install curl -y")
-        subprocess.run("curl -fsSL https://repo.cider.sh/APT-GPG-KEY | sudo gpg --dearmor -o /usr/share/keyrings/cider-archive-keyring.gpg")
-        subprocess.run('echo "deb [signed-by=/usr/share/keyrings/cider-archive-keyring.gpg] https://repo.cider.sh/apt stable main" | sudo tee /etc/apt/sources.list.d/cider.list')
-        subprocess.run("sudo apt update")
-        subprocess.run("sudo apt install cider")
-        subprocess.run('echo "Cider installed successfully"')
+        subprocess.run(["sudo", "apt", "install", "curl" "-y"])
+        subprocess.run(["curl", "-fsSL", "https://repo.cider.sh/APT-GPG-KEY", "|", "sudo", "gpg", "--dearmor", "-o", "/usr/share/keyrings/cider-archive-keyring.gpg"])
+        subprocess.run(["echo", '"deb [signed-by=/usr/share/keyrings/cider-archive-keyring.gpg] https://repo.cider.sh/apt stable main"', "|", "sudo", "tee", "/etc/apt/sources.list.d/cider.list"])
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "install", "cider"])
+        subprocess.run(["echo", '"Cider installed successfully"'])
     except:
         print("Install Failed")

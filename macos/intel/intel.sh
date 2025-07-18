5143c740a87470d312a9f759f3459cacfdfbee0c
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-18 00:19:12 -0400

    fixing macos bugs

--- macos/intel/intel.sh
+++ macos/intel/intel.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Selected Intel..."
+curl "https://github.com/skylarkoningin/cider_install_scripts/releases/download/1.0.0/cider-v3.0.2-macos-x64.dmg"
 hdiutil attach ./cider-v3.0.2-macos-x64.dmg
-cp -R /Volumes/Cider\ Installer\ (x64)/Cider.app /Applications/
-hdiutil detach /Volumes/Cider\ Installer\ (x64)/
+cp -R "/Volumes/Cider Installer (x64)/Cider.app" /Applications/
+hdiutil detach "/Volumes/Cider Installer (x64)/"
 
--- macos/silicon/silicon.sh
+++ macos/silicon/silicon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Selected Silicon..."
+curl "https://github.com/skylarkoningin/cider_install_scripts/releases/download/1.0.0/cider-v3.0.2-macos-arm64.dmg"
 hdiutil attach ./cider-v3.0.2-macos-arm64.dmg
-cp -R /Volumes/Cider\ Installer\ (arm64)/Cider.app /Applications/
-hdiutil detach /Volumes/Cider\ Installer\ (arm64)/
+cp -R "/Volumes/Cider Installer (arm64)/Cider.app" /Applications/
+hdiutil detach "/Volumes/Cider Installer (arm64)/"

b0496c643fe17eb4dab7a29bcb3bfbf247565110
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-18 00:26:42 -0400

    fixing macos bugs

--- macos/silicon/silicon.sh
+++ macos/silicon/silicon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl "https://github.com/skylarkoningin/cider_install_scripts/releases/download/1.0.0/cider-v3.0.2-macos-arm64.dmg"
+curl -L -o "https://github.com/skylarkoningin/cider_install_scripts/releases/download/1.0.0/cider-v3.0.2-macos-arm64.dmg"
 hdiutil attach ./cider-v3.0.2-macos-arm64.dmg
 cp -R "/Volumes/Cider Installer (arm64)/Cider.app" /Applications/
 hdiutil detach "/Volumes/Cider Installer (arm64)/"

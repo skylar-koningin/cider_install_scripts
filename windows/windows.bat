3c94aa3d31779833ebf52ab473b2b8d326ef7a6a
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-29 20:31:17 -0400

    fixing bugs

---
+++ windows/windows.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo Selected Windows...
+move "%USERPROFILE%\Downloads\cider-v3.0.0-windows-x64.exe" .\
+cider-v3.0.0-windows-x64.exe
--- windows/windows.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Selected Windows..."
-mv ~/Downloads/cider-v3.0.0-windows-x64.exe ./
-./cider-v3.0.0-windows-x64.exe

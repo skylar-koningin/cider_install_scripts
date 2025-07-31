6dcfcac3bf6e61ff18c11a11626b81203b5af7f4
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-31 09:36:45 -0400

    Update xbps.sh

--- linux/xbps/xbps.sh
+++ linux/xbps/xbps.sh
@@ -5,9 +5,9 @@ sudo xbps-install curl
 mv ~/Downloads/cider-v3.0.2-linux-x64.AppImage ~/cider_install_scripts/linux/xbps
 chmod +x ./cider-v3.0.2-linux-x64.AppImage
 mkdir ~/CiderApp
+mkdir ~/.local/applications
 mv ./cider-v3.0.2-linux-x64.AppImage ~/CiderApp
 mv ./cider.png ~/CiderApp
-touch ~/.local/share/applications/CiderApp/CiderApp.desktop
 echo "[Desktop Entry]
     Name=Cider
     Exec=~/CiderApp/cider-v3.0.2-linux-x64.AppImage

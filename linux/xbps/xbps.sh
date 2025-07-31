a3bbfa35f25691db4646fa5c20dcbd04e8c5dc60
Skylar Koningin <greekie.via.linux@gmail.com>
2025-07-31 16:59:18 -0400

    Update xbps.sh

--- linux/xbps/xbps.sh
+++ linux/xbps/xbps.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "Selected xbps..."
-sudo xbps-install curl
 mv ~/Downloads/cider-v3.0.2-linux-x64.AppImage ~/cider_install_scripts/linux/xbps
 chmod +x ./cider-v3.0.2-linux-x64.AppImage
 mkdir ~/CiderApp

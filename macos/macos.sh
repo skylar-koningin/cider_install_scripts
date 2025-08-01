549a9517f324ec36570aaefebcc8e4b0f8c78499
Skylar Koningin <greekie.via.linux@gmail.com>
2025-08-01 04:16:23 -0400

    Update macos.sh

--- macos/macos.sh
+++ macos/macos.sh
@@ -5,21 +5,26 @@ mac_cpu() {
     echo "What CPU does your Mac have?"
     echo "1. Intel"
     echo "2. Silicon"
-    read -p "Select a CPU (choose 0 to abort): " os
+    echo "3. Other"
+    read -p "Select a CPU (choose 0 to abort): " cpu
 }
 
 checker() {
-    if [ $os = 0 ]; then
+    if [ $cpu = 0 ]; then
         echo "Aborting install..."
         exit 0
 
-    elif [ $os = 1 ]; then
+    elif [ $cpu = 1 ]; then
         cd ./intel
         sh intel.sh
 
-    elif [ $os = 2 ]; then
+    elif [ $cpu = 2 ]; then
         cd ./silicon
         sh silicon.sh
+
+    elif [ $cpu = 3 ]; then
+        echo "What the fuck kinda cpu are you running on, MacOS only supports x86_64/AMD64 (Intel) and ARM64/aarch64 (Silicon)"
+        exit 0
 
     else
         echo "Invalid option, choose again."

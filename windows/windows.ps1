b53209b2a352545ae11e8423e99b592aefe427d0
Skylar Koningin <greekie.via.linux@gmail.com>
2025-08-03 09:08:10 -0400

    Update windows.ps1

--- windows/windows.ps1
+++ windows/windows.ps1
@@ -1,4 +1,4 @@
 Write-Host Selected Windows...
 Move-Item -Path "$env:USERPROFILE\Downloads\cider-v3.0.0-windows-x64.exe" -Destination "."
-winget install --id Microsoft.DotNet.DesktopRuntime.9 -e --accept-package-agreements --accept-source-agreements
+winget install --id Microsoft.DotNet.DesktopRuntime.8 -e --accept-package-agreements --accept-source-agreements
 Start-Process -FilePath ".\cider-v3.0.0-windows-x64.exe"
